9f6d33669ca7604ea67ca8870330eac409088779
pankajkoree <pankajkori2057@gmail.com>
2024-11-16 00:35:24 +0530

    start page

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -12,13 +12,14 @@ const Start: React.FC = () => {
     router.push("/quizPage");
   };
   return (
-    <div className="relative flex flex-col">
-      <div className="relative flex xl:-top-28">
-        <Image src={logo} alt="Logo" className="xl:w-[500px]" />
+    <div className="relative flex-col w-full">
+      <div className="relative flex justify-center xl:top-4">
+        <Image src={logo} alt="Logo" className="xl:w-[400px]" />
       </div>
-      <div className="relative flex justify-center xl:-top-16">
+
+      <div className="relative flex justify-center xl:top-24">
         <button
-          className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-yellow-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-blue-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-200 focus:ring-offset-2 focus:ring-offset-slate-50 xl:text-2xl"
+          className="inline-flex h-16 animate-shimmer items-center justify-center border border-yellow-700 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-8 font-medium text-blue-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-200 focus:ring-offset-2 focus:ring-offset-slate-50 xl:text-3xl hover:border hover:border-red-500 hover:scale-110 shadow-md shadow-green-400"
           onClick={gotoQuizPage}
         >
           Start Now
@@ -29,7 +30,13 @@ const Start: React.FC = () => {
           />
         </button>
       </div>
-      <div className="relative flex top-40 justify-center">
+
+      <div className="relative flex justify-center xl:top-44 xl:text-2xl dark:text-white">
+        <h2>
+          Highest score : <span>20/25</span>
+        </h2>
+      </div>
+      <div className="relative flex justify-center xl:top-[37%]">
         <Footer />
       </div>
     </div>
