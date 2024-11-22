c8d3f73c3caad4924f3cde07b07918756e119a4f
pankajkoree <pankajkori2057@gmail.com>
2024-11-22 20:15:41 +0530

    score page

--- src/app/scorePage/page.tsx
+++ src/app/scorePage/page.tsx
@@ -1,12 +1,32 @@
 import Image from "next/image";
 import logo from "../../../public/file.png";
+import score from "../../../public/score.png";
+import Footer from "../Footer";
 
 const scorePage = () => {
   return (
-    <div>
+    <div className="">
+      {/* div for the logo */}
       <div>
         <Image src={logo} alt="logo" className="xl:w-[200px]" />
       </div>
+      {/* end div for the logo */}
+
+      {/* div for the score bar */}
+      <div>
+        <Image src={score} alt="score" className="xl:w-[200px]" />
+      </div>
+      {/* end div for the score bar */}
+
+      {/* retry button */}
+      <div>
+        <button className="text-white">Retry</button>
+      </div>
+      {/* end of retry button */}
+
+      {/* footer */}
+      <Footer />
+      {/* footer */}
     </div>
   );
 };
